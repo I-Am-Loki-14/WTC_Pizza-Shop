7d88c5b2744b37f44dabe2f28f2258586a3d888a
Min Naing Oo <minnaingoo792@gmail.com>
2026-05-02 19:48:31 +0630

    form

--- js/javascript.js
+++ js/javascript.js
@@ -26,3 +26,33 @@ function prevCarousel (){
 
 }
 setInterval(()=>nextCarousel(),3000)
+
+
+// form 
+
+const form = document.querySelector("#form")
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+
+  const formData = new FormData(this);
+  const data = Object.fromEntries(formData.entries())
+  let isValid = true;
+  const validations ={
+            name:data.name.trim() != "",
+            email:data.email.trim() != "",
+            subject:data.subject.trim() != "",
+            message:data.message.trim() != "",
+  };
+  for(field in validations){
+    if(!validations[field]){
+        document.getElementById(field).style.borderColor = "red";
+        isValid = false;
+    }else {
+        document.getElementById(field).style.borderColor = "#202020"
+    }
+  }
+  if (isValid) {
+    alert("SUCCESS! Message successfully sent");
+    this.reset();
+  }
+})
